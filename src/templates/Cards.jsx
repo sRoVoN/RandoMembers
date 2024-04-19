4307f31918a54f300f7a248e4e47b1814cdc0aa0
Soodeh Arvin <soodeh.arvin@gmail.com>
2024-04-19 12:01:35 +0330

    Debug Slide in Cards.

--- src/templates/Cards.jsx
+++ src/templates/Cards.jsx
@@ -6,48 +6,72 @@ import {
   CardMedia,
   CardActions,
   Button,
+  Slide,
 } from "@mui/material";
 import { Grid } from "@mui/material";
+import { useEffect } from "react";
 
-export const Cards = ({ member }) => {
+export const Cards = ({
+  member,
+  loading = { loading },
+  setLoading = { setLoading },
+  index = { index },
+}) => {
+  useEffect(() => {
+    setLoading(true);
+
+    console.log(setLoading(true), "ser");
+
+    return () => {
+      setLoading(false);
+    };
+  }, []);
   return (
-    <Grid
-      display="flex"
-      sx={{ justifyContent: "center", textAlign: "center" }}
-      spacing={2}
+    <Slide
+      direction="up"
+      in={loading}
+      style={{
+        transitionDelay: loading ? `${index + 1}99ms` : "0ms",
+      }}
     >
-      <Card
-        sx={{
-          maxWidth: 300,
-          backgroundColor: "lightblue",
-          mr: 2,
-          p: 2,
-          mb: 2,
-          justifyContent: "center",
-          textAlign: "center",
-          alignItems: "center",
-        }}
+      <Grid
+        display="flex"
+        sx={{ justifyContent: "center", textAlign: "center" }}
+        spacing={2}
       >
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            height="100"
-            width="30"
-            image={member.picture.large}
-            alt={member.name.last}
-          />
-          <CardContent>
-            <Typography variant="h6">{`${member.name.first} ${member.name.last} `}</Typography>
-            <Typography variant="body2">{member.location.contrey}</Typography>
-            <Typography variant="body2">{member.location.city}</Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <Button hre="" size="small" color="primary" target="_blank">
-            more information
-          </Button>
-        </CardActions>
-      </Card>
-    </Grid>
+        <Card
+          sx={{
+            maxWidth: 300,
+            backgroundColor: "lightblue",
+            mr: 2,
+            p: 2,
+            mb: 2,
+            justifyContent: "center",
+            textAlign: "center",
+            alignItems: "center",
+          }}
+        >
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="100"
+              width="30"
+              image={member.picture.large}
+              alt={member.name.last}
+            />
+            <CardContent>
+              <Typography variant="h6">{`${member.name.first} ${member.name.last} `}</Typography>
+              <Typography variant="body2">{member.location.contrey}</Typography>
+              <Typography variant="body2">{member.location.city}</Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button hre="" size="small" color="primary" target="_blank">
+              more information
+            </Button>
+          </CardActions>
+        </Card>
+      </Grid>
+    </Slide>
   );
 };
