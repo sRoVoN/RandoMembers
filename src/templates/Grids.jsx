2b024b615090ac598bdea1f46c6e9b8fe93f264e
Soodeh Arvin <soodeh.arvin@gmail.com>
2024-04-19 12:03:29 +0330

    Debug Slide in Grids.

--- src/templates/Grids.jsx
+++ src/templates/Grids.jsx
@@ -1,8 +1,24 @@
-import { Box, Typography, Avatar } from "@mui/material";
+import { Box, Typography, Avatar, Slide } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2";
+import { useEffect } from "react";
 
-export const Grids = ({ member }) => {
+export const Grids = ({ member, index, loading, setLoading }) => {
+  useEffect(() => {
+    setLoading(true);
+
+    return() => {
+      setLoading(false);
+    }
+
+  }, []);
   return (
+    <Slide
+    direction="left"
+    in={loading}
+    style={{
+      transitionDelay: loading ? `${index + 1}99ms` : "0ms"
+    }}
+    >
     <Grid sx={{ justifyContent: "center", textAlign: "center" }}>
       <Box sx={{ width: 200, height: 200, borderRadius: "50%", m: 2 }}>
         <Avatar
@@ -17,5 +33,6 @@ export const Grids = ({ member }) => {
         <Typography variant="body2">{member.location.country}</Typography>
       </Box>
     </Grid>
+    </Slide>
   );
 };
